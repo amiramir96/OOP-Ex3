a94b2b9d713be7a456394180f4f5180f1e20f9dd
amiramir96 <89981387+amiramir96@users.noreply.github.com>
2021-12-23 23:28:27 +0200

    we have dijsktra ^^ with tests!

---
+++ tests/test_Dijkstra.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from src.GraphAlgo import GraphAlgo
+
+
+class Test(TestCase):
+    def test_dijkstra(self):
+        """
+        cases:
+            1,2 - ordinary, shortest path in graph
+            3 - ask for shortest path (use dijkstra) while one of the nodes isnt exists in the graph
+            4 - both of nodes is not exists in the graph
+        """
+        algo = GraphAlgo()
+        # case 1:
+        algo.load_from_json(r'data\A0.json')
+        ans = algo.shortest_path(8, 3)
+        w_ans = 7.282598961846713
+        self.assertEqual(ans[0], w_ans)
+        # case 2:
+        algo.load_from_json(r'data\A4.json')
+        ans = algo.shortest_path(0, 5)
+        w_ans = 6.834625309281369
+        self.assertEqual(ans[0], w_ans)
+        # case 3:
+        algo.get_graph().remove_node(0)
+        ans = algo.shortest_path(5, 0)
+        w_ans = float('inf')
+        self.assertEqual(ans[0], w_ans)
+        ans = algo.shortest_path(123812, 3)
+        self.assertEqual(ans[0], w_ans)
+        # case 4:
+        ans = algo.shortest_path(12031, 12391)
+        self.assertEqual(ans[0], w_ans)

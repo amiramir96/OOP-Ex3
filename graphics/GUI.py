5cb4895d9c21a00b791663534f97ca89eea0a814
Ori Darshan <77589259+oridarshan@users.noreply.github.com>
2021-12-26 00:57:13 +0200

    3/4 buttons!

--- graphics/GUI.py
+++ graphics/GUI.py
@@ -1,8 +1,8 @@
 import math
-
 import pygame
 from src.GraphAlgoInterface import GraphAlgoInterface
 from src.GraphAlgo import GraphAlgo
+from Button import Button
 
 # initialize pygame and font (singletons)
 pygame.init()
@@ -29,9 +29,6 @@ def a_b_given_c_m(c, m):
     """
     a^2 + b^2 = c^2
     get a,b when c and his slope (m) are known
-    :param c:
-    :param m:
-    :return:
     """
     # b = m*a,   c^2 = a^2 + (m*a)^2,  a^2 = (c^2)/(1+m^2)
     a = math.sqrt((c**2)/(1+m**2))
@@ -56,7 +53,7 @@ def draw_arrow(p1: tuple, p2: tuple, color: (int, int, int) = (0, 0, 0), radius:
     triangle_point1 = (triangle_cut_point[0] + x, triangle_cut_point[1] + y)
     triangle_point2 = (triangle_cut_point[0] - x, triangle_cut_point[1] - y)
     # draw line
-    pygame.draw.line(screen, color, (x1, y1), (x2, y2), int(radius/10))
+    pygame.draw.line(screen, color, (x1, y1), (x2, y2), math.ceil(radius/10))
     pygame.draw.polygon(screen, color, [(x2, y2), triangle_point1, triangle_point2])
 
 
@@ -70,56 +67,121 @@ def plot(g: GraphAlgoInterface):
     min_y = min(nodes, key=lambda n: n[1][1])[1][1]
     max_y = max(nodes, key=lambda n: n[1][1])[1][1]
 
+    center_point_button = Button("CENTER POINT")
+    center_point_button.set_click_listener('center_point')
+    shortest_path_button = Button("SHORTEST PATH")
+    shortest_path_button.set_click_listener('shortest_path')
+    reset_button = Button("RESET")
+
+    chosen_nodes = []
+    color_nodes = []
+    color_edges = []
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit(0)
 
+            if center_point_button.check(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+                color_nodes.append(center_point_button.action(g, center_point_button))
+
+            if shortest_path_button.check(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+                if len(chosen_nodes) == 2:
+                    path_nodes = shortest_path_button.action(g, shortest_path_button, chosen_nodes[0], chosen_nodes[1])
+                    for i in range(len(path_nodes)-1):
+                        color_edges.append((path_nodes[i], path_nodes[i+1]))
+                else:  # reset
+                    chosen_nodes = []
+                    shortest_path_button.action(shortest_path_button)
+
+            # reset
+            if reset_button.check(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+                chosen_nodes = []
+                color_nodes = []
+                color_edges = []
+                center_point_button.reset()
+                shortest_path_button.reset()
+
+
         screen.fill(pygame.Color(239, 228, 176))
         # scale radius of nodes
         r = scale(200, 0, screen.get_width(), 0, WIDTH) / len(nodes)
+        FONT = pygame.font.SysFont('Ariel', max(int(r), 10))
+        margin = screen.get_width()/20  # screen margins
         # paint nodes and edges
         for node in nodes:
             node_id = node[0]
+            if node_id in color_nodes:
+                color = (34, 177, 76)
+            elif node_id in chosen_nodes:
+                color = (163, 73, 164)
+            else:
+                color = (63, 72, 204)
+
             x, y = node[1][0], node[1][1]
             # scale position according to screen
-            x = scale(x, 50, screen.get_width()-50, min_x, max_x)
-            y = scale(y, 150, screen.get_height()-50, min_y, max_y)
-            pygame.draw.circle(screen, pygame.Color(63, 72, 204), (x, y), r)
-            # draw edges
-            for edge in g.get_graph().all_out_edges_of_node(node[0]).values():
-                dest_x = g.get_graph().get_all_v()[edge[0]][1][0]
-                dest_y = g.get_graph().get_all_v()[edge[0]][1][1]
-                dest_x = scale(dest_x, 50, screen.get_width() - 50, min_x, max_x)
-                dest_y = scale(dest_y, 150, screen.get_height() - 50, min_y, max_y)
-                # pygame.draw.line(screen, pygame.Color(0,0,0),(x,y),(dest_x,dest_y),4)
-                draw_arrow((x, y), (dest_x, dest_y), radius=r)
+            x = scale(x, margin, screen.get_width()-margin, min_x, max_x)
+            y = scale(y, 3*margin, screen.get_height()-margin, min_y, max_y)
+            node_circle = pygame.Rect((x-r, y-r), (2*r, 2*r))
+            if node_circle.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+                if node_id not in chosen_nodes:
+                    chosen_nodes.append(node_id)
+            pygame.draw.circle(screen, color, (x, y), r)
+            # draw id
+            id_srf = FONT.render(str(node_id), True, (0, 0, 0))
+            id_rect = id_srf.get_rect(center=(x, y-1.5*r))
+            screen.blit(id_srf, id_rect)
 
+        # draw all edges
         for node in nodes:
             node_id = node[0]
             x, y = node[1][0], node[1][1]
             # scale position according to screen
-            x = scale(x, 50, screen.get_width()-50, min_x, max_x)
-            y = scale(y, 150, screen.get_height()-50, min_y, max_y)
+            x = scale(x, margin, screen.get_width()-margin, min_x, max_x)
+            y = scale(y, 3*margin, screen.get_height()-margin, min_y, max_y)
             # pygame.draw.circle(screen, pygame.Color(63, 72, 204), (x, y), r)
             # draw edges
             for edge in g.get_graph().all_out_edges_of_node(node[0]).values():
+                color = (0, 0, 0)
+                if (node_id, g.get_graph().get_all_v()[edge[0]][0]) in color_edges:
+                    color = (0, 162, 232)
                 dest_x = g.get_graph().get_all_v()[edge[0]][1][0]
                 dest_y = g.get_graph().get_all_v()[edge[0]][1][1]
-                dest_x = scale(dest_x, 50, screen.get_width() - 50, min_x, max_x)
-                dest_y = scale(dest_y, 150, screen.get_height() - 50, min_y, max_y)
+                dest_x = scale(dest_x, margin, screen.get_width() - margin, min_x, max_x)
+                dest_y = scale(dest_y, 3*margin, screen.get_height() - margin, min_y, max_y)
                 # pygame.draw.line(screen, pygame.Color(0,0,0),(x,y),(dest_x,dest_y),4)
-                draw_arrow((x, y), (dest_x, dest_y))
+                draw_arrow((x, y), (dest_x, dest_y), color, radius=r)
 
-        # drawing an arrow:
-        # pygame.draw.polygon(screen, (0, 0, 0),
-        #                     ((0, 100), (0, 200), (200, 200), (200, 300), (300, 150), (200, 0), (200, 100)))
+        # draw special edges
+        for node in nodes:
+            node_id = node[0]
+            x, y = node[1][0], node[1][1]
+            # scale position according to screen
+            x = scale(x, margin, screen.get_width()-margin, min_x, max_x)
+            y = scale(y, 3*margin, screen.get_height()-margin, min_y, max_y)
+            # pygame.draw.circle(screen, pygame.Color(63, 72, 204), (x, y), r)
+            # draw edges
+            for edge in g.get_graph().all_out_edges_of_node(node[0]).values():
+                if (node_id, g.get_graph().get_all_v()[edge[0]][0]) in color_edges:
+                    color = (0, 162, 232)
+                    dest_x = g.get_graph().get_all_v()[edge[0]][1][0]
+                    dest_y = g.get_graph().get_all_v()[edge[0]][1][1]
+                    dest_x = scale(dest_x, margin, screen.get_width() - margin, min_x, max_x)
+                    dest_y = scale(dest_y, 3*margin, screen.get_height() - margin, min_y, max_y)
+                    # pygame.draw.line(screen, pygame.Color(0,0,0),(x,y),(dest_x,dest_y),4)
+                    draw_arrow((x, y), (dest_x, dest_y), color, radius=r)
+
+        # draw buttons
+        center_point_button.render(screen, (margin/5, margin/5), (margin*4, margin*1.5))
+        reset_button.render(screen, (screen.get_width()-margin/5-margin*4, margin/5), (margin*4, margin*1.5))
+        shortest_path_button.render(screen, (2*(margin/5) + margin*4, margin/5), (margin*4, margin*1.5))
+
         pygame.display.update()
         clock.tick(60)
 
 
 if __name__ == '__main__':
     g = GraphAlgo()
-    g.load_from_json('data\\square.json')
+    g.load_from_json('data\\A1.json')
     plot(g)
